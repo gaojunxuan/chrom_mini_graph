bb862875cb0f07200048ffcd1d70a31417481a06
Jun Gao <gaojunxuanbox@live.com>
2023-07-14 01:40:10 -0400

    Update benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -21,12 +21,5 @@ SIM_FOLDER=/u/jgao/jgao/projects/chrom_minigraph/sim
 
 for i in ${SIZES[@]}
 do
-    $CMG_FOLDER/run_generate.sh "$SIM_FOLDER/${i}/refs/*.fasta" &> $CMG_FOLDER/log_generate_size_${i}.txt
-    for j in {0..9}
-    do
-        $CMG_FOLDER/run_map.sh $GENERATED_FILE "$SIM_FOLDER/${i}/reads/ref${j}_len${i}.fasta.fastq" &> $CMG_FOLDER/log_map_size_${i}_ref_${j}.txt
-        $SAMTOOLS sort $BAM_FILE -o $SORTED_BAM
-        $SAMTOOLS coverage $SORTED_BAM > $CMG_FOLDER/coverage_report_size_${i}_ref_${j}.txt
-        $PYTHON3 $CMG_FOLDER/analyze_mapping_accuracy.py ref${j}_len${i} $CMG_FOLDER/best_genome_reads.txt > accuracy_report_size_${i}_ref_${j}.txt
-    done
+    $QSUB -N benchmark_size_$i $CMG_FOLDER/benchmark_given_size.sh $i
 done
---
+++ benchmark_given_size.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#$ -P simpsonlab
+#$ -cwd
+#$ -l h_vmem=64G
+#$ -l h_rt=144000
+#$ -m eas
+#$ -M jgao@oicr.on.ca
+
+CMG_FOLDER=/u/jgao/jgao/code/chrom_mini_graph
+CMG=/u/jgao/jgao/code/chrom_mini_graph/target/debug/chrom_mini_graph
+GENERATED_FILE=/u/jgao/jgao/code/chrom_mini_graph/serialized_mini_graph.bin
+BAM_FILE=/u/jgao/jgao/code/chrom_mini_graph/output.bam
+SORTED_BAM=/u/jgao/jgao/code/chrom_mini_graph/sorted_output.bam
+PYTHON3=/u/jgao/miniconda3/bin/python3
+SAMTOOLS=/.mounts/labs/simpsonlab/sw/samtools/1.17/bin/samtools
+QSUB=/opt/uge-8.6/bin/lx-amd64/qsub
+SIM_FOLDER=/u/jgao/jgao/projects/chrom_minigraph/sim
+
+$CMG_FOLDER/run_generate.sh "$SIM_FOLDER/$1/refs/*.fasta" &> $CMG_FOLDER/report/$1/logs/gen/log_generate_size_${$1}.txt
+for j in {0..9}
+do
+    mkdir $CMG_FOLDER/report/$1
+    mkdir $CMG_FOLDER/report/$1/logs
+    mkdir $CMG_FOLDER/report/$1/logs/gen
+    mkdir $CMG_FOLDER/report/$1/logs/map
+    $CMG_FOLDER/run_map.sh $GENERATED_FILE $SIM_FOLDER/$1/reads/ref${j}_len$1.fasta.fastq &> $CMG_FOLDER/report/$1/logs/map/log_map_size_$1_ref_${j}.txt
+    $SAMTOOLS sort $BAM_FILE -o $SORTED_BAM
+    $SAMTOOLS coverage $SORTED_BAM > $CMG_FOLDER/report/$1/coverage_report_size_$1_ref_${j}.txt
+    $PYTHON3 $CMG_FOLDER/analyze_mapping_accuracy.py ref${j}_len$1 $CMG_FOLDER/best_genome_reads.txt > $CMG_FOLDER/report/$1/accuracy_report_size_$1_ref_${j}.txt
+done
